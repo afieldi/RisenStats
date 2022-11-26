586ac07fc54345f2405ac78a9bd8a8ff2b652ddf
vexrax <joshuaf_111@live.com>
2022-11-26 00:18:34 -0500

    Allow for reverse leaderboard

--- front-end/src/components/player-page/stats/stats-generators/DeathPercentStatGenerator.ts
+++ front-end/src/components/player-page/stats/stats-generators/DeathPercentStatGenerator.ts
@@ -2,6 +2,12 @@ import {BaseStatGenerator} from "./BaseStatsGenerator";
 import PlayerStatModel from "../../../../../../Common/models/playerstat.model";
 
 export class DeathPercentStatGenerator extends BaseStatGenerator {
+
+    constructor() {
+        super();
+        this.shouldInvertLeaderboard = true;
+    }
+
     getStatTitle(): string {
         return "Death %"
     }
