3b8c5016b285b4ba2a7c8eaf3fc758fd81c03441
vexrax <joshuaf_111@live.com>
2022-11-27 11:56:50 -0500

    Fix some compiler warnings

--- front-end/src/components/player-page/stats/win-rate-box.tsx
+++ front-end/src/components/player-page/stats/win-rate-box.tsx
@@ -1,8 +1,8 @@
 import RisenBox1 from "../../risen-box/risen-box-1";
-import {Cell, Label, Pie, PieChart, ResponsiveContainer, Tooltip} from "recharts";
+import {Cell, Label, Pie, PieChart, Tooltip} from "recharts";
 import React from "react";
 import {useTheme} from "@emotion/react";
-import {Divider, Theme, Typography} from "@mui/material";
+import {Theme, Typography} from "@mui/material";
 import "./win-rate-box.css"
 import {calculateWR} from "../../../../../Common/utils";
 
@@ -15,8 +15,8 @@ interface WinRateBoxProps {
 export default function WinRateBox(winRateProps: WinRateBoxProps) {
     const theme = useTheme() as Theme;
 
-    const data = [  {name: 'Wins', value: winRateProps.wins, color: theme.palette.primary.dark},
-                    {name: 'Losses', value: winRateProps.losses, color: theme.palette.secondary.dark}]
+    const data = [{name: 'Wins', value: winRateProps.wins, color: theme.palette.primary.dark},
+        {name: 'Losses', value: winRateProps.losses, color: theme.palette.secondary.dark}]
     const winRate = calculateWR({totalWins: winRateProps.wins, totalGames: winRateProps.wins + winRateProps.losses}, 1)
     const winsOverLoss = `${winRateProps.wins + winRateProps.losses}G ${winRateProps.wins}G ${winRateProps.losses}L`
 
@@ -36,11 +36,11 @@ export default function WinRateBox(winRateProps: WinRateBoxProps) {
                     <Label value={winRate + "%"}
                            position="centerBottom"
                            className='label-top'
-                           style={{ fill: theme.palette.info.dark, fontSize: '35px' }}/>
+                           style={{fill: theme.palette.info.dark, fontSize: '35px'}}/>
                     <Label value={winsOverLoss}
                            position="centerTop"
                            className='label'
-                           style={{ fill: theme.palette.info.dark }}/>
+                           style={{fill: theme.palette.info.dark}}/>
                     {
                         data.map((entry, index) => <Cell key={index} fill={entry.color}/>)
                     }
