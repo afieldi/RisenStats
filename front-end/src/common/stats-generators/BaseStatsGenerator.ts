2c62157e1180e9b911f83053fa10c8a85e17abcf
Arek <arek.fielding@gmail.com>
2022-12-03 18:15:35 -0500

    Changes to leaderboards and how stats are calculated

--- front-end/src/common/stats-generators/BaseStatsGenerator.ts
+++ front-end/src/common/stats-generators/BaseStatsGenerator.ts
@@ -39,13 +39,15 @@ export abstract class BaseStatGenerator {
     }
 
     getStatString(playerStatsModels: PlayerStatModel[]): string {
-        let total = 0
-        let roles = 0;
+        let total = 0;
+        let games = 0;
         for (let playerStatsModel of playerStatsModels) {
-            total += this.getStatValue(playerStatsModel)
-            roles += 1
+            const weight1 = games / (games + playerStatsModel.games);
+            const weight2 = 1 - weight1;
+            total = (total * weight1) + (weight2 * this.getStatValue(playerStatsModel));
+            games += playerStatsModel.games;
         }
-        return `${this.formatNumber(total/roles)}` ;
+        return `${this.formatNumber(total)}` ;
     }
 
     getSortedLeaderboard(unsortedLeaderboard: PlayerStatModel[]): PlayerStatModel[] {
@@ -53,5 +55,14 @@ export abstract class BaseStatGenerator {
             return this.getStatValue(o2) - this.getStatValue(o1);
         })
         return this.shouldInvertLeaderboard ?  sortedLeaderboard.reverse() : sortedLeaderboard;
+    }
+
+    getPositionInLeaderboard(value: number, sortedLeaderboard: PlayerStatModel[]): number {
+        for (const i in sortedLeaderboard) {
+            if (value > this.getStatValue(sortedLeaderboard[i])) {
+                return +i+1;
+            }
+        }
+        return sortedLeaderboard.length + 1;
     }
 }
--- front-end/src/components/player-page/stats/performance-overview.tsx
+++ front-end/src/components/player-page/stats/performance-overview.tsx
@@ -53,21 +53,17 @@ function getStatBox(index: number, statGenerator: BaseStatGenerator, performance
 
     const average = sorted.reduce((total, next) => total + statGenerator.getStatValue(next), 0) / sorted.length;
 
-    let rank = 0;
-    let isPlayerInLeaderBoard = false;
-    for(; rank < sorted.length; rank++) {
-        if(sorted[rank].playerPuuid === performanceOverviewProps.playerPuuid) {
-            isPlayerInLeaderBoard = true;
-            break;
-        }
-    }
+    let rank = statGenerator.getPositionInLeaderboard(
+        +statGenerator.getStatString(performanceOverviewProps.playerStats),
+        sorted
+    );
 
     return <StatBox key={index}
                     statToolTip={statGenerator.getToolTip()}
                     statValue={statGenerator.getStatString(performanceOverviewProps.playerStats)}
                     statTitle={statGenerator.getStatTitle()}
                     haveStatsLoaded={statGenerator.canLoadData(performanceOverviewProps.playerStats)}
-                    shouldShowLeaderboard={isPlayerInLeaderBoard && statGenerator.canLoadData(performanceOverviewProps.playerStats)}
+                    shouldShowLeaderboard={sorted.length > 0 && statGenerator.canLoadData(performanceOverviewProps.playerStats)}
                     leaderboardData={{
                         rank: rank + 1,
                         leagueAvg: average,
