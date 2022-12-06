e10f72cb885b815d5eb9e2e33e4ffbe3c75c6563
vexrax <joshuaf_111@live.com>
2022-12-05 20:19:13 -0500

    Map rank to color

--- front-end/src/components/player-page/stats/game-rating.tsx
+++ front-end/src/components/player-page/stats/game-rating.tsx
@@ -13,6 +13,15 @@ interface GameRatingProps {
 export default function GameRating(props: GameRatingProps) {
     const theme = useTheme() as Theme;
 
+    const colorMap: Record<Rank, string> = {
+        "S+": theme.palette.primary.light,
+        S: theme.palette.primary.dark,
+        A: theme.palette.first.main,
+        B: theme.palette.second.main,
+        C: theme.palette.third.main,
+        D: theme.palette.info.light,
+    }
+
     return (
         <Box sx={{maxWidth: 280, display: 'flex', flexDirection: 'column', rowGap: 2}}>
             <RisenBox1 sx={{minWidth: 230}}>
@@ -20,7 +29,7 @@ export default function GameRating(props: GameRatingProps) {
                 <Divider sx={{marginBottom: 2}}/>
                 <Box sx={{display: 'flex', flexDirection: 'row', columnGap: 1, justifyContent: "space-between", alignContent: "center"}}>
                     <Typography sx={{paddingTop: "10px"}} color={theme.palette.info.light} align="center" variant="h6">{props.rating}</Typography>
-                    <Typography  color={theme.palette.primary.dark} align="center" variant="h3">{props.rank}</Typography>
+                    <Typography  color={colorMap[props.rank]} align="center" variant="h3">{props.rank}</Typography>
                 </Box>
             </RisenBox1>
         </Box>
