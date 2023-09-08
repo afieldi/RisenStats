c152de324c2c4c809c213328d7b5591edf46ded1
vexrax <joshuaf_111@live.com>
2023-09-08 18:47:13 -0400

    Setup the parser to account for new sheets.

--- BackEnd/src/business/sheets/premadeParser.ts
+++ BackEnd/src/business/sheets/premadeParser.ts
@@ -25,25 +25,30 @@ export class PremadeParser implements RisenSheetParser {
   public buildTeam(row: any[]): RisenTeam {
     return {
       teamName: row[0],
-      abrv: row[2],
-      win: row[4],
-      loss: row[5],
-      top: row[7],
-      jungle: row[8],
-      mid: row[9],
-      adc: row[10],
-      support: row[11],
-      sub1: row[12] ?? undefined,
-      sub2: row[13] ?? undefined,
-      sub3: row[14] ?? undefined,
-      sub4: row[15] ?? undefined,
-      sub5: row[16] ?? undefined,
+      abrv: row[1],
+      win: row[2],
+      loss: row[3],
+      top: row[5],
+      jungle: row[6],
+      mid: row[7],
+      adc: row[8],
+      support: row[9],
+      sub1: row[10] ?? undefined,
+      sub2: row[11] ?? undefined,
+      sub3: row[12] ?? undefined,
+      sub4: row[13] ?? undefined,
+      sub5: row[14] ?? undefined,
     };
   }
 
   isValidRow(data: string[]): boolean {
     // Check to make sure there's enough columns and make sure the name and abbr exist
-    return data.length > 5 && isValidRowData(data[0]) && isValidRowData(data[2]);
+    return data.length > 5 && isValidRowData(data[0]) && isValidRowData(data[1]) && this.hasValidOPGG(data[15]);
+  }
+
+  hasValidOPGG(url: string): boolean {
+    const regex = /^http:\/\/na\.op\.gg\/multi/;
+    return regex.test(url);
   }
 }
 
