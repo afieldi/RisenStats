86639cfa87a1c3f24b3c3070bc0502794588d1e6
Vexrax <joshuaf_111@live.com>
2024-01-22 19:05:55 -0500

    Fix risen sheet parser and update some stuff

--- BackEnd/scripts/buildRisenTeams.ts
+++ BackEnd/scripts/buildRisenTeams.ts
@@ -3,11 +3,11 @@ dotenv.config({ path: '../.env.development' });
 import { buildRisenTeams } from '../src/business/teams';
 
 /*
-    22 - champions
-    23 - Diviine
-    24 - draft
-    25 - dom
-    26 - unstop
-    27 - ramp
+    ?? - champions
+    28 - Mythical
+    ?? - draft
+    ?? - dom
+    ?? - unstop
+    ?? - ramp
  */
-buildRisenTeams(27);
+buildRisenTeams(28);
