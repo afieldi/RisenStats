0946f5b7e9b2b69c1cc9f7a11248ee9a40d534ff
Vexrax <joshuaf_111@live.com>
2024-11-23 12:46:07 -0500

    Support draft league with teams

---
+++ BackEnd/src/business/sheets/parserHelpers.ts
@@ -0,0 +1,30 @@
+import { PlayerIdentifier } from './sheets';
+import log from '../../../logger';
+
+export function hasValidOPGG(url: string): boolean {
+  const regex = /^http:\/\/na\.op\.gg\/multi/;
+  return regex.test(url);
+}
+
+export function getPlayerIdentifierFromString(inputString: string): PlayerIdentifier {
+  if (!inputString) {
+    return undefined;
+  }
+
+  if(!inputString.includes('#')) {
+    log.error(`Player name, ${inputString} was invalid!, no # to seperate!`);
+    return undefined;
+  }
+
+  let splitString = inputString.split('#');
+
+  if (splitString.length != 2) {
+    log.error(`Player name, ${inputString} was invalid!`);
+    return undefined;
+  }
+
+  return {
+    gameName: splitString[0].trim(),
+    tagline: splitString[1].trim()
+  };
+}
--- BackEnd/src/business/sheets/premadeParser.ts
+++ BackEnd/src/business/sheets/premadeParser.ts
@@ -1,5 +1,5 @@
-import log from '../../../logger';
-import { PlayerIdentifier, RisenSheetParser, RisenTeam } from './sheets';
+import {  RisenSheetParser, RisenTeam } from './sheets';
+import { getPlayerIdentifierFromString, hasValidOPGG } from './parserHelpers';
 
 export class PremadeParser implements RisenSheetParser {
   public buildTeam(row: any[]): RisenTeam {
@@ -8,56 +8,27 @@ export class PremadeParser implements RisenSheetParser {
       abrv: row[1],
       win: row[2],
       loss: row[3],
-      top: this.getPlayerIdentifierFromString(row[5]),
-      jungle: this.getPlayerIdentifierFromString(row[6]),
-      mid: this.getPlayerIdentifierFromString(row[7]),
-      adc: this.getPlayerIdentifierFromString(row[8]),
-      support: this.getPlayerIdentifierFromString(row[9]),
-      sub1: this.getPlayerIdentifierFromString(row[10]) ?? undefined,
-      sub2: this.getPlayerIdentifierFromString(row[11]) ?? undefined,
-      sub3: this.getPlayerIdentifierFromString(row[12]) ?? undefined,
-      sub4: this.getPlayerIdentifierFromString(row[13]) ?? undefined,
-      sub5: this.getPlayerIdentifierFromString(row[14]) ?? undefined,
+      top: getPlayerIdentifierFromString(row[5]),
+      jungle: getPlayerIdentifierFromString(row[6]),
+      mid: getPlayerIdentifierFromString(row[7]),
+      adc: getPlayerIdentifierFromString(row[8]),
+      support: getPlayerIdentifierFromString(row[9]),
+      sub1: getPlayerIdentifierFromString(row[10]) ?? undefined,
+      sub2: getPlayerIdentifierFromString(row[11]) ?? undefined,
+      sub3: getPlayerIdentifierFromString(row[12]) ?? undefined,
+      sub4: getPlayerIdentifierFromString(row[13]) ?? undefined,
+      sub5: getPlayerIdentifierFromString(row[14]) ?? undefined,
     };
   }
 
   public isValidRow(data: string[]): boolean {
     // Check to make sure there's enough columns and make sure the name and abbr exist
     // If a row has a opgg then we know its a team row and not a filler row.
-    return data.length > 5 && this.isValidRowData(data[0]) && this.isValidRowData(data[1]) && this.hasValidOPGG(data[15]);
+    return data.length > 5 && this.isValidRowData(data[0]) && this.isValidRowData(data[1]) && hasValidOPGG(data[15]);
   }
 
   isValidRowData(data: string): boolean {
     return data && data.length > 1;
   }
-
-  hasValidOPGG(url: string): boolean {
-    const regex = /^http:\/\/na\.op\.gg\/multi/;
-    return regex.test(url);
-  }
-
-  getPlayerIdentifierFromString(inputString: string): PlayerIdentifier {
-    if (!inputString) {
-      return undefined;
-    }
-
-    if(!inputString.includes('#')) {
-      log.error(`Player name, ${inputString} was invalid!, no # to seperate!`);
-      return undefined;
-    }
-
-    let splitString = inputString.split('#');
-
-    if (splitString.length != 2) {
-      log.error(`Player name, ${inputString} was invalid!`);
-      return undefined;
-    }
-
-    return {
-      gameName: splitString[0].trim(),
-      tagline: splitString[1].trim()
-    };
-  }
-
 }
 
