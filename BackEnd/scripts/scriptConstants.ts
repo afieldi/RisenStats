45f114d268d6c8ab5e3a7fcfa501e014d13aed5f
Vexrax <joshuaf_111@live.com>
2025-01-25 16:58:16 -0500

    Updae scripts to support doing OE work. Update stocks to be non deterministic so we can run a script to rebuild them

--- BackEnd/scripts/scriptConstants.ts
+++ BackEnd/scripts/scriptConstants.ts
@@ -2,6 +2,6 @@
 export const RAMPAGE: number = 46;
 export const UNSTOPPABLE: number = 45;
 export const DOMINATE: number = 44;
-export const DRAFT: number = 42;
+export const DRAFT: number = 50;
 export const MYTHICAL: number = 43;
 export const CHAMPIONS: number = -1;
